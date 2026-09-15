936838507c31f278b992bd20bb003b08fca7693c
crisvilo <cdviloria25@gmail.com>
2026-09-15 07:01:50 -0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,66 @@
+/*
+ * Service Worker de "Gestión de Cartera"
+ */
+
+const CACHE_VERSION = 'v1';
+const CACHE_NAME = `cartera-cache-${CACHE_VERSION}`;
+
+const APP_SHELL = [
+  './',
+  './index.html',
+  './css/styles.css',
+  './js/app.js',
+  './img/favicon.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(APP_SHELL))
+      .then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys()
+      .then((keys) =>
+        Promise.all(
+          keys
+            .filter((key) => key !== CACHE_NAME)
+            .map((key) => caches.delete(key))
+        )
+      )
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  const { request } = event;
+
+  if (
+    request.method !== 'GET' ||
+    new URL(request.url).origin !== self.location.origin
+  ) {
+    return;
+  }
+
+  event.respondWith(
+    fetch(request)
+      .then((response) => {
+        const copy = response.clone();
+
+        caches.open(CACHE_NAME)
+          .then((cache) => cache.put(request, copy));
+
+        return response;
+      })
+      .catch(() => caches.match(request))
+  );
+});
+
+self.addEventListener('message', (event) => {
+  if (event.data === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
